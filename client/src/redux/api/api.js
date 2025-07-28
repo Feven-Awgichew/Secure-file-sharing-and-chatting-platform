5ec98881664da2ee7e97769e5132865f0e54221c
Feven Gurju <135051593+Feven-Awgichew@users.noreply.github.com>
2025-07-28 11:36:05 +0300

    Add files via upload

--- client/src/redux/api/api.js
+++ client/src/redux/api/api.js
@@ -1,1 +1,115 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+
+const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:5000/api/` }),
+  tagTypes: ["Chat", "User", "Message"],
+
+  endpoints: (builder) => ({
+    myChats: builder.query({
+      query: () => ({
+        url: "chat/my-chats",
+        credentials: "include",
+      }),
+      providesTags: ["Chat"],
+    }),
+
+    searchUser: builder.query({
+      query: (name) => ({
+        url: `user/search?name=${name}`,
+        credentials: "include",
+      }),
+      providesTags: ["User"],
+    }),
+
+   
+
+  
+
+    chatDetails: builder.query({
+      query: ({ chatId, populate = false }) => {
+        let url = `chat/${chatId}`;
+        if (populate) url += "?populate=true";
+
+        return {
+          url,
+          credentials: "include",
+        };
+      },
+      providesTags: ["Chat"],
+    }),
+
+    getMessages: builder.query({
+      query: ({ chatId, page }) => ({
+        url: `chat/messages/${chatId}?page=${page}`,
+        credentials: "include",
+      }),
+      keepUnusedDataFor: 0,
+    }),
+
+    sendAttachments: builder.mutation({
+      query: (data) => ({
+        url: "chat/message",
+        method: "POST",
+        credentials: "include",
+        body: data,
+      }),
+    }),
+
+    
+
+    getAdminStats: builder.query({
+      query: () => ({
+        url: "admin/stats",
+        credentials: "include",
+      }),
+    }),
+
+    getAllUsers: builder.query({
+      query: () => ({
+        url: "admin/users",
+        credentials: "include",
+      }),
+    }),
+
+    getAllChats: builder.query({
+      query: () => ({
+        url: "admin/chats",
+        credentials: "include",
+      }),
+    }),
+
+    getAllMessages: builder.query({
+      query: () => ({
+        url: "admin/messages",
+        credentials: "include",
+      }),
+    }),
+  }),
+});
+
+export default api;
+
+export const {
+  useMyChatsQuery,
+  useLazySearchUserQuery,
+  useSendFriendRequestMutation,
+  useGetNotificationsQuery,
+  useAcceptFriendRequestMutation,
+  useChatDetailsQuery,
+  useGetMessagesQuery,
+  useSendAttachmentsMutation,
+  useMyGroupsQuery,
+  useAvailableFriendsQuery,
+  useNewGroupMutation,
+  useRenameGroupMutation,
+  useRemoveGroupMemberMutation,
+  useAddGroupMembersMutation,
+  useDeleteChatMutation,
+  useLeaveGroupMutation,
+  useGetAdminStatsQuery,
+  useGetAllUsersQuery,
+  useGetAllChatsQuery,
+  useGetAllMessagesQuery,
+} = api;
